5f0d102309c9899e72323979fc9355989efc8001
Saurabh Dubey <72212856+saurabh-dubey98@users.noreply.github.com>
2022-09-29 19:52:59 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,14 +4,6 @@ import { getStorage } from "firebase/storage"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-	// apiKey: "AIzaSyCB9OMTNMgmK6pV-BJqW_izn6UULnbhCPo",
-	// authDomain: "ppmexplorepackage-maker.firebaseapp.com",
-	// projectId: "ppmexplorepackage-maker",
-	// storageBucket: "ppmexplorepackage-maker.appspot.com",
-	// messagingSenderId: "374619828396",
-	// appId: "1:374619828396:web:732bc57974d76bf3a8ef09",
-	// measurementId: "G-LFHBY4LCV1",
-
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
